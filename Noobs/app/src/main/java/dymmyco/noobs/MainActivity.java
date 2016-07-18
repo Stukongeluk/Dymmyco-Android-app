ce1c2c52ab4dee272188884c033cba9fe5c23f2b
Dylan de Wit <s1096777@student.hsleiden.nl>
2016-07-18 16:46:45 +0200

    fgt2.4

--- Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
+++ Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
@@ -1,15 +1,32 @@
 package dymmyco.noobs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Dylan on 18-7-2016.
  */
 public class MainActivity extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
+        setContentView(R.layout.inlogscherm);
+
+    Button login = (Button) findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText name = (EditText) findViewById(R.id.editText);
+                Intent i = new Intent(getApplicationContext(), SchermTweeActivity.class);
+                i.putExtra("Naam", name.getText().toString());
+                startActivity(i);
+            }
+        });
     }
 }
--- Noobs/app/src/main/java/dymmyco/noobs/SchermTweeActivity.java
+++ Noobs/app/src/main/java/dymmyco/noobs/SchermTweeActivity.java
@@ -1,7 +1,22 @@
 package dymmyco.noobs;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by Dylan on 18-7-2016.
  */
-public class SchermTweeActivity {
+public class SchermTweeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tweedescherm);
+        String naam = getIntent().getStringExtra("Naam");
+
+        TextView name = (TextView) findViewById(R.id.textView);
+        name.setText(naam);
+
+    }
 }
