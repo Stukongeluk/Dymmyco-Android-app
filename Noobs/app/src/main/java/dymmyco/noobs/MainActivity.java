fda72384b89289849f1e1e6e911377a28b933054
Dylan de Wit <s1096777@student.hsleiden.nl>
2016-07-18 15:05:39 +0200

    fgt2.1

--- Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
+++ Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
@@ -1,7 +1,15 @@
 package dymmyco.noobs;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Dylan on 18-7-2016.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
 }
