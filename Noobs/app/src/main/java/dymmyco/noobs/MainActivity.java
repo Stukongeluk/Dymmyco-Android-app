a55ab18eaefd2a752c0f9a4b7803f960e967f10d
Dylan de Wit <s1096777@student.hsleiden.nl>
2016-07-18 20:22:26 +0200

    menutje/tabs toegevoegd

--- Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
+++ Noobs/app/src/main/java/dymmyco/noobs/MainActivity.java
@@ -3,6 +3,8 @@ package dymmyco.noobs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,14 +12,14 @@ import android.widget.EditText;
 /**
  * Created by Dylan on 18-7-2016.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inlogscherm);
 
-    Button login = (Button) findViewById(R.id.button);
+        Button login = (Button) findViewById(R.id.button);
         login.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -28,5 +30,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
--- Noobs/app/src/main/java/dymmyco/noobs/SchermTweeActivity.java
+++ Noobs/app/src/main/java/dymmyco/noobs/SchermTweeActivity.java
@@ -2,6 +2,8 @@ package dymmyco.noobs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 /**
@@ -17,6 +19,12 @@ public class SchermTweeActivity extends AppCompatActivity {
 
         TextView name = (TextView) findViewById(R.id.textView);
         name.setText(naam);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
